40c8c114d97e6fb8d20011eb90e498705df40870
Matija Rudnik <matija_ru@yahoo.com>
2018-09-12 16:16:13 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+    el: "#show-tvshowes",
+    data:{
+        shows: [],
+        search: ""
+        
+    },
+    methods:{
+             
+    },
+    created(){
+    this.$http.get("http://api.tvmaze.com/shows?q=").then(function(data){
+        console.log(data);
+        this.shows = data.body;
+    })
+    },
+    computed: {
+        filteredShows: function(){
+            return this.shows.filter((show) => {
+                return show.name.match(this.search);
+            });
+        }
+    }
+    
+})
+
