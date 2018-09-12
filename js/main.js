2762012759f625d34a805c4722ce2f59ace0f9dd
Matija Rudnik <matija_ru@yahoo.com>
2018-09-12 16:27:58 +0200

    added new comments

--- js/main.js
+++ js/main.js
@@ -3,17 +3,15 @@ var app = new Vue({
     data:{
         shows: [],
         search: ""
-        
     },
-    methods:{
-             
-    },
+
     created(){
-    this.$http.get("http://api.tvmaze.com/shows?q=").then(function(data){
-        console.log(data);
-        this.shows = data.body;
-    })
-    },
+        this.$http.get("http://api.tvmaze.com/shows?q=").then(function(data){
+            console.log(data);
+            this.shows = data.body;
+        })
+    },    
+
     computed: {
         filteredShows: function(){
             return this.shows.filter((show) => {
